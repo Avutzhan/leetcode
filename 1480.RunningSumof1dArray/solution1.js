06875a43b5c662efaaf5774e24334ec58eb8a3a5
Avutzhan Dautov <dautov92@list.ru>
2020-12-28 12:09:26 +0600

    running sum of 1d array

--- 1480.RunningSumof1dArray/solution1.js
+++ 1480.RunningSumof1dArray/solution1.js
@@ -1,8 +1,12 @@
 //solution 1
 const runningSum = nums => {
+    // console.log(nums, "input array");
     nums.reduce((a,c,i,arr) => arr[i] += a)
     return nums
 }
+
+runningSum([1,2,3,4]); //[1,3,6,10]
+// Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4]
 
 // a: accumulator
 // c: current value
