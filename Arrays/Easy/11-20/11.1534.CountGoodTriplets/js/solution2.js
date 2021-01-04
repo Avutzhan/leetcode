6a71f0acbc802c60d07e2fcb45f16eb5098ca672
Avutzhan Dautov <dautov92@list.ru>
2021-01-04 09:56:58 +0600

    Count Good Triplets js solution 2

---
+++ Arrays/Easy/11-20/11.1534.CountGoodTriplets/js/solution2.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} arr
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
+ * @return {number}
+ */
+var countGoodTriplets = function(arr, a, b, c) {
+    var res = 0;
+    var n = arr.length;
+    for (var i = 0; i < n - 2; i++) {
+        for (var j = i + 1; j < n - 1; j++) {
+            for (var k = j + 1; k < n; k++) {
+                if (Math.abs(arr[i] - arr[j]) <= a && Math.abs(arr[j] - arr[k]) <= b && Math.abs(arr[k] - arr[i]) <= c) res++;
+            }
+        }
+    }
+    return res;
+};
