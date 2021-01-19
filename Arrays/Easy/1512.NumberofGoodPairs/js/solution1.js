ddada29fb2bbd9e750d21a86bb3a149ce2efb651
Avutzhan Dautov <dautov92@list.ru>
2021-01-19 15:24:53 +0600

    Number of Good Pairs

---
+++ Arrays/Easy/1512.NumberofGoodPairs/js/solution1.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function numIdenticalPairs(nums) { // O(n^2)
+    let sum = 0;
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] === nums[j]) sum++;
+        }
+    }
+    return sum;
+}
