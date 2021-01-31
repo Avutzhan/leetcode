58ec8bf74b9113a953cb4ffb6764b15e3e04ca27
Avutzhan Dautov <dautov92@list.ru>
2021-01-31 18:58:58 +0600

    Maximum Product of Two Elements in an Array

---
+++ Arrays/Easy/11-20/19.1464.MaximumProductofTwoElementsinanArray/python/solution1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        m1 = 0
+        m2 = 0
+
+        for i in range(len(nums)):
+            m2 = max(m2, min(m1, nums[i]))
+            m1 = max(m1, nums[i])
+
+        return (m1 - 1) * (m2 - 1);
+
+        
