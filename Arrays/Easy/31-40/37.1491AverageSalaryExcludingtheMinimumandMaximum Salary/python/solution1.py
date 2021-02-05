4f61f28a4f503ea974506243ca750cf9bb93d09c
Avutzhan Dautov <dautov92@list.ru>
2021-02-05 16:36:41 +0600

    Average Salary Excluding the Minimum and Maximum Salary

---
+++ Arrays/Easy/31-40/37.1491AverageSalaryExcludingtheMinimumandMaximum Salary/python/solution1.py
@@ -0,0 +1,7 @@
+class Solution:
+    def average(self, salary: List[int]) -> float:
+        m = min(salary)
+        x = max(salary)
+        s = sum(salary) - m - x
+
+        return s / (len(salary) - 2)
