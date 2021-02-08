8e57e2302294db9bd1c03521b333556c9a76bb49
Avutzhan Dautov <dautov92@list.ru>
2021-02-08 16:54:11 +0600

    Defuse the Bomb

--- Arrays/Easy/51-60/54.1652DefusetheBomb/js/solution1.js
+++ Arrays/Easy/51-60/54.1652DefusetheBomb/js/solution1.js
@@ -1,10 +1,33 @@
 /**
- * @param {number[]} nums
+ * @param {number[]} code
+ * @param {number} k
  * @return {number[]}
  */
-var frequencySort = function(A) {
-    let m = new Map();
-    A.forEach(x => m.set(x, 1 + (m.get(x) || 0)));
-    let P = [...m.entries()].sort((a, b) => a[1] == b[1] ? b[0] - a[0] : a[1] - b[1]);
-    return _.flatten(P.map(([x, cnt]) => Array(cnt).fill(x)));
+var decrypt = function(code, k) {
+    var res = new Array(code.length).fill(0)
+    if (k > 0){
+        for(var i = 0; i < code.length; i++){
+            var count = 0
+            var j = i + 1
+            while(count < k){
+                if (j === code.length) j = 0
+                res[i] += code[j]
+                count = count + 1
+                j++
+            }
+        }
+    }
+    if (k < 0){
+        for(var i = 0; i < code.length; i++){
+            var count = 0
+            var j = i - 1
+            while(count > k){
+                if (j === -1) j = code.length - 1
+                res[i] += code[j]
+                count = count - 1
+                j--
+            }
+        }
+    }
+    return res
 };
