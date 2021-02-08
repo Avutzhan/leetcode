bfa9c502827450fcc817b1cba96310d23ebc130c
Avutzhan Dautov <dautov92@list.ru>
2021-02-08 16:42:58 +0600

    Special Positions in a Binary Matrix

--- Arrays/Easy/51-60/52.1582SpecialPositionsinaBinaryMatrix/js/solution1.js
+++ Arrays/Easy/51-60/52.1582SpecialPositionsinaBinaryMatrix/js/solution1.js
@@ -1,31 +1,21 @@
 /**
- * @param {character[][]} board
+ * @param {number[][]} mat
  * @return {number}
  */
-var canCapture = function(board, i, j) {
-    let captures = 0, dirs = [[1, 0], [-1, 0], [0, 1], [0, -1]];
-
-    for (let dir of dirs) {
-        let r = i + dir[0], c = j + dir[1];
-
-        while (r >= 0 && r < 8 && c >= 0 && c < 8) {
-            if (board[r][c] === 'p') { captures++; }
-            if (board[r][c] !== '.') { break; }
-
-            r += dir[0];
-            c += dir[1];
-        }
-    }
-
-    return captures
-};
-
-const numRookCaptures = board => {
-    for (let i = 0; i < 8; i++) {
-        for (let j = 0; j < 8; j++) {
-            if (board[i][j] === 'R') {
-                return canCapture(board, i, j);
-            }
-        }
-    }
+var numSpecial = function(A) {
+    let M = A.length,
+        N = A[0].length,
+        cnt = 0;
+    let row = Array(M).fill(0),
+        col = Array(N).fill(0);
+    for (let i = 0; i < M; ++i)
+        for (let j = 0; j < N; ++j)
+            if (A[i][j])
+                ++row[i],
+                    ++col[j];
+    for (let i = 0; i < M; ++i)
+        for (let j = 0; j < N; ++j)
+            if (A[i][j] && row[i] == 1 && col[j] == 1)
+                ++cnt;
+    return cnt;
 };
