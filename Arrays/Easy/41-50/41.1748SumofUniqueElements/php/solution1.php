3b1220d42d6919a71cc7957365b4ed4001d39720
Avutzhan Dautov <dautov92@list.ru>
2021-02-10 18:28:30 +0600

    Sum of Unique Elements php and puthon solutions

--- Arrays/Easy/41-50/41.1748SumofUniqueElements/php/solution1.php
+++ Arrays/Easy/41-50/41.1748SumofUniqueElements/php/solution1.php
@@ -1,2 +1,23 @@
 <?php
 
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function sumOfUnique($nums) {
+        $sum =0;
+        $a = $nums;
+        foreach ($a as $t) {
+            $count =0;
+
+            for ($q =0;$q<count($nums);$q++){
+                if($nums[$q]==$t) $count++;
+            }
+
+            if($count<=1) $sum+=$t;
+        }
+        return $sum;
+    }
+}
