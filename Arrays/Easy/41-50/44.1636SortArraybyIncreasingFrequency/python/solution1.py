73d8b02e1be994f4cf3bef1189648caa78b5cb5a
Avutzhan Dautov <dautov92@list.ru>
2021-02-12 14:03:34 +0600

    Sort Array by Increasing Frequency python sol 2

---
+++ Arrays/Easy/41-50/44.1636SortArraybyIncreasingFrequency/python/solution1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def frequencySort(self, nums: List[int]) -> List[int]:
+        # 0) Collect all number and its frequency
+        fv_dict = {}  # key as number, value as [frequency, number]
+        for n in nums:
+            if n in fv_dict:
+                fv_dict[n][0] += 1
+            else:
+                fv_dict[n] = [1, n]
+
+        # 1) Sorting them
+        sorted_fv = sorted(fv_dict.values(), key=lambda t: (t[0], t[1]*-1))
+        # default, sorted in descending order by frequency and then tne number itself
+
+        # 2) Compose the answer
+        ans = []
+        for f, v in sorted_fv:
+            ans.extend([v]*f)
+
+        # 3) Return it
+        return ans
