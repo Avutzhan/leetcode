fcc9ab200e0c9956c642846d1854dddfee8aaffa
Avutzhan Dautov <dautov92@list.ru>
2021-02-15 12:59:57 +0600

    Count Largest Group php sol

--- Arrays/Easy/41-50/49.1399CountLargestGroup/php/solution1.php
+++ Arrays/Easy/41-50/49.1399CountLargestGroup/php/solution1.php
@@ -1,2 +1,35 @@
 <?php
 
+class Solution {
+
+    /**
+     * @param Integer $n
+     * @return Integer
+     */
+    function countLargestGroup($n) {
+        $m = [];
+        $max = 0;
+
+        for ($i = 1; $i <= $n; $i++) {
+            $sum = $this->getSum($i);
+            if(!isset($m[$sum])) {
+                $m[$sum] = 0;
+            }
+            $m[$sum]++;
+            $max = max($max, $m[$sum]);
+        }
+
+
+        return count(array_keys($m, max($m)));
+    }
+
+    function getSum($n) {
+        $sum = 0;
+        while ($n > 0) {
+            $sum += $n % 10;
+            $n = floor($n / 10);
+        }
+        return $sum;
+    }
+
+}
