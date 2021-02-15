09b011a789a199f129cdf9cc3eaedaebebf6ae1e
Avutzhan Dautov <dautov92@list.ru>
2021-02-15 19:24:41 +0600

    Special Positions in a Binary Matrix php sol

--- Arrays/Easy/51-60/52.1582SpecialPositionsinaBinaryMatrix/php/solution1.php
+++ Arrays/Easy/51-60/52.1582SpecialPositionsinaBinaryMatrix/php/solution1.php
@@ -1,2 +1,37 @@
 <?php
 
+class Solution {
+
+    /**
+     * @param Integer[][] $mat
+     * @return Integer
+     */
+    function numSpecial($mat) {
+        $M = count($mat);
+        $N = count($mat[0]);
+        $cnt = 0;
+        $row = array_fill(0, $M, 0);
+        $col = array_fill(0, $N, 0);
+        for ($i = 0; $i < $M; $i++) {
+            for ($j = 0; $j < $N; $j++) {
+                if ($mat[$i][$j]) {
+                    $row[$i]++;
+                    $col[$j]++;
+                }
+
+            }
+
+        }
+
+        for ($i = 0; $i < $M; $i++) {
+            for ($j = 0; $j < $N; $j++) {
+                if ($mat[$i][$j] && $row[$i] == 1 && $col[$j] == 1) {
+                    $cnt++;
+                }
+            }
+
+        }
+
+        return $cnt;
+    }
+}
