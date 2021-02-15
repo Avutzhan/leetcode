c0d421718b1eaf66ffbd5d8ca70a07330152d124
Avutzhan Dautov <dautov92@list.ru>
2021-02-15 16:40:50 +0600

    Three Consecutive Odds clear sol 1

---
+++ Arrays/Easy/51-60/51.1550ThreeConsecutiveOdds/js/clear_sol1.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var threeConsecutiveOdds = function(arr) {
+    for (let i = 0, x = 0; i < arr.length; i++) {
+        // x = arr[i] % 2 ? x | [1, 2, 4][i % 3] : 0;
+        if (arr[i] % 2) {
+            x = x | [1, 2, 4][i % 3];
+        } else {
+            x = 0;
+        }
+
+        if (7 === x) return true;
+    }
+    return false;
+};
