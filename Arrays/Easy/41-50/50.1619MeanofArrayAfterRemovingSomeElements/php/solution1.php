10e50025ec4ac31dfdbd1c61921962c4dcd12618
Avutzhan Dautov <dautov92@list.ru>
2021-02-15 14:26:21 +0600

    Mean of Array After Removing Some Elements php

--- Arrays/Easy/41-50/50.1619MeanofArrayAfterRemovingSomeElements/php/solution1.php
+++ Arrays/Easy/41-50/50.1619MeanofArrayAfterRemovingSomeElements/php/solution1.php
@@ -1,2 +1,17 @@
 <?php
 
+class Solution {
+
+    /**
+     * @param Integer[] $arr
+     * @return Float
+     */
+    function trimMean($arr) {
+        $N = count($arr);
+        $K = intval($N / 20);
+        sort($arr);
+        $sum = array_sum(array_slice($arr,$K,$N-$K*2));
+
+        return $sum/($N-2*$K);
+    }
+}
