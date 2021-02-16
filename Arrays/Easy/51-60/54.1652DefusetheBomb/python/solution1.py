933b2a4aa7a9a528a4ebf4ea84e64aefae2db64c
Avutzhan Dautov <dautov92@list.ru>
2021-02-16 15:45:01 +0600

    Defuse the Bomb python sol

---
+++ Arrays/Easy/51-60/54.1652DefusetheBomb/python/solution1.py
@@ -0,0 +1,28 @@
+class Solution:
+    def decrypt(self, code: List[int], k: int) -> List[int]:
+        res = [0] * len(code)
+
+        if k > 0:
+            for i in range(len(code)):
+                count = 0
+                j = i + 1
+                while count < k:
+                    if j == len(code):
+                        j = 0
+                    res[i] += code[j]
+                    count = count + 1
+                    j+=1
+
+        if k < 0:
+            for i in range(len(code)):
+                count = 0
+                j = i - 1
+                while count > k:
+                    if j == -1:
+                        j = len(code) - 1
+                    res[i] += code[j]
+                    count = count - 1
+                    j-=1
+
+        return res
+        
