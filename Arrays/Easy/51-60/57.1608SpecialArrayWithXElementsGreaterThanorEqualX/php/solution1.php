63a161dab6de1bed5d06fc4a7e47dae0d86e1b87
Avutzhan Dautov <dautov92@list.ru>
2021-02-16 18:40:33 +0600

    Special Array With X Elements Greater Than or Equal X php sol

--- Arrays/Easy/51-60/57.1608SpecialArrayWithXElementsGreaterThanorEqualX/php/solution1.php
+++ Arrays/Easy/51-60/57.1608SpecialArrayWithXElementsGreaterThanorEqualX/php/solution1.php
@@ -1,2 +1,22 @@
 <?php
 
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function specialArray($nums) {
+        for ($i = 0; $i <= count($nums); $i++) {
+            $c = 0;
+            foreach ($nums as $num) {
+                $num >= $i && $c++;
+            }
+
+            if ($c === $i) {
+                return $i;
+            }
+        }
+        return -1;
+    }
+}
