2be290986baefa6f07e3240dac33dab672a9498f
Avutzhan Dautov <dautov92@list.ru>
2021-02-16 15:56:17 +0600

    Check If All 1's Are at Least Length K Places Away php sol

--- Arrays/Easy/51-60/55.1437CheckIfAll1sAreatLeastLengthKPlacesAway/php/solution1.php
+++ Arrays/Easy/51-60/55.1437CheckIfAll1sAreatLeastLengthKPlacesAway/php/solution1.php
@@ -1,2 +1,20 @@
 <?php
 
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $k
+     * @return Boolean
+     */
+    function kLengthApart($nums, $k) {
+        for ($i=-$k-1,  $j=0; $j<count($nums); $j++) {
+            if ($nums[$j] == 1) {
+                if ($j-$i-1 < $k )
+                    return false;
+                $i = $j;
+            }
+        }
+        return true;
+    }
+}
