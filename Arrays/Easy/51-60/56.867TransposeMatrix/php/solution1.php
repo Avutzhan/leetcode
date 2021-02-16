f4a71808c583d66e23b345e00aa08b630636c90f
Avutzhan Dautov <dautov92@list.ru>
2021-02-16 16:46:38 +0600

    Transpose Matrix php sol

--- Arrays/Easy/51-60/56.867TransposeMatrix/php/solution1.php
+++ Arrays/Easy/51-60/56.867TransposeMatrix/php/solution1.php
@@ -1,2 +1,21 @@
 <?php
 
+class Solution {
+
+    /**
+     * @param Integer[][] $matrix
+     * @return Integer[][]
+     */
+    function transpose($A) {
+        $res = [];
+
+        for ($i = 0; $i < count($A[0]); $i++) {
+            $res[$i] = [];
+            for ($j = 0; $j < count($A); $j++) {
+                $res[$i][$j] = $A[$j][$i];
+            }
+        }
+
+        return $res;
+    }
+}
