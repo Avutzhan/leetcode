f4dbeba9c2674befc1770ca085e390aa14c6f6b7
Avutzhan Dautov <dautov92@list.ru>
2021-02-18 21:20:10 +0600

    Most Visited Sector in a Circular Track php sol

--- Arrays/Easy/71-80/71.1560MostVisitedSectorinaCircularTrack/php/solution1.php
+++ Arrays/Easy/71-80/71.1560MostVisitedSectorinaCircularTrack/php/solution1.php
@@ -1,2 +1,33 @@
 <?php
 
+class Solution {
+
+    /**
+     * @param Integer $n
+     * @param Integer[] $rounds
+     * @return Integer[]
+     */
+    function mostVisited($n, $rounds) {
+        $start = $rounds[0];
+        $end = $rounds[count($rounds)-1];
+
+        $result = [];
+
+        if($start <= $end) {
+            for($i = $start; $i <= $end; $i++)  {
+                $result[] = $i;
+            }
+
+        } else {
+            for($i = 1; $i <= $end; $i++) {
+                $result[] = $i;
+            }
+
+            for($i = $start; $i <= $n; $i++) {
+                $result[] = $i;
+            }
+        }
+
+        return $result;
+    }
+}
