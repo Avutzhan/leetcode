2dbbef2332cc39e5c799e6c729c9b5fc896f9f6c
Avutzhan Dautov <dautov92@list.ru>
2021-02-18 17:51:48 +0600

    Best Time to Buy and Sell Stock II php sol

--- Arrays/Easy/61-70/68.122BestTimetoBuyandSellStockII/php/solution1.php
+++ Arrays/Easy/61-70/68.122BestTimetoBuyandSellStockII/php/solution1.php
@@ -1,2 +1,23 @@
 <?php
 
+class Solution {
+
+    /**
+     * @param Integer[] $prices
+     * @return Integer
+     */
+    function maxProfit($prices) {
+        $profit = 0;
+
+        for ($i = 1; $i < count($prices); $i++) {
+            $prev = $prices[$i - 1];
+            $current = $prices[$i];
+
+            if ($prev < $current) {
+                $profit += $current - $prev;
+            }
+        }
+
+        return $profit;
+    }
+}
