72bbb3caf651e8c80db7e49930a89fc57ab3ff6c
Avutzhan Dautov <dautov92@list.ru>
2021-02-18 16:47:44 +0600

    Fair Candy Swap optimal php sol

--- Arrays/Easy/61-70/66.888FairCandySwap/php/solution1.php
+++ Arrays/Easy/61-70/66.888FairCandySwap/php/solution1.php
@@ -1,2 +1,31 @@
 <?php
 
+class Solution {
+
+    /**
+     * @param Integer[] $A
+     * @param Integer[] $B
+     * @return Integer[]
+     */
+    function fairCandySwap($A, $B) {
+        $suma = array_reduce($A, function($carry, $item) {
+            $carry += $item;
+            return $carry;
+        });
+
+        $sumb = array_reduce($B, function($carry, $item) {
+            $carry += $item;
+            return $carry;
+        });
+
+        $diff = ($suma - $sumb) / 2;
+
+        for ($i=0; $i< count($B); $i++){
+            $curr = $B[$i] + $diff;
+
+            if (in_array($curr, $A)) {
+                return [$curr, $B[$i]];
+            }
+        }
+    }
+}
