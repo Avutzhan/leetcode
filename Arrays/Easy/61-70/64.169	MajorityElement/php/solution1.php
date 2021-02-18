e9dad9d33754b412ec1a04dae0f2e03591b78c15
Avutzhan Dautov <dautov92@list.ru>
2021-02-18 14:04:58 +0600

    Majority Element php sol

--- Arrays/Easy/61-70/64.169	MajorityElement/php/solution1.php
+++ Arrays/Easy/61-70/64.169	MajorityElement/php/solution1.php
@@ -1,2 +1,14 @@
 <?php
 
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function majorityElement($nums) {
+        sort($nums);
+        $cnt = floor(count($nums)) / 2;
+        return $nums[$cnt];
+    }
+}
