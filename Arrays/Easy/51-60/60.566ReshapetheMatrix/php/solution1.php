3a9d65aaa198a991b13e37111d562369c418fd67
Avutzhan Dautov <dautov92@list.ru>
2021-02-18 11:21:43 +0600

    Reshape the Matrix php sol 2

--- Arrays/Easy/51-60/60.566ReshapetheMatrix/php/solution1.php
+++ Arrays/Easy/51-60/60.566ReshapetheMatrix/php/solution1.php
@@ -1,2 +1,32 @@
 <?php
 
+class Solution {
+
+    /**
+     * @param Integer[][] $nums
+     * @param Integer $r
+     * @param Integer $c
+     * @return Integer[][]
+     */
+    function matrixReshape($nums, $h, $w) {
+        $m = count($nums);
+        $n = count($nums[0]);
+
+        if ($m * $n !== $h * $w) {
+            return $nums;
+        }
+
+
+        $res = [];
+        for ($i = 0, $r = 0; $r < $m; $r++) {
+            for ($c = 0; $c < $n; $c++, $i++) {
+                $rr = floor($i / $w);
+                if (!$res[$rr]) {
+                    array_push($res, []);
+                }
+                array_push($res[$rr], $nums[$r][$c]);
+            }
+        }
+        return $res;
+    }
+}
