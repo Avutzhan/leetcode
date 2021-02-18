24290679d721f7b772433958aed242f97f3e2f30
Avutzhan Dautov <dautov92@list.ru>
2021-02-18 13:48:07 +0600

    Element Appearing More Than 25% In Sorted Array php sol

--- Arrays/Easy/61-70/63.1287ElementAppearingMoreThan25InSortedArray/php/solution1.php
+++ Arrays/Easy/61-70/63.1287ElementAppearingMoreThan25InSortedArray/php/solution1.php
@@ -1,2 +1,19 @@
 <?php
 
+class Solution {
+
+    /**
+     * @param Integer[] $arr
+     * @return Integer
+     */
+    function findSpecialInteger($arr) {
+        $ws = floor(count($arr) / 4);
+
+        for ($i = 0; $i < count($arr) - $ws; $i++) {
+            if ($arr[$i] === $arr[$i + $ws]) {
+                return $arr[$i];
+            }
+        }
+        return -1;
+    }
+}
