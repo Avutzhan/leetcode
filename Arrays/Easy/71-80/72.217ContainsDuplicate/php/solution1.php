13ba0ab1cb1efb88ab3776e9f4b336c2e3d71c37
Avutzhan Dautov <dautov92@list.ru>
2021-02-18 21:43:24 +0600

    Contains Duplicate php sol

--- Arrays/Easy/71-80/72.217ContainsDuplicate/php/solution1.php
+++ Arrays/Easy/71-80/72.217ContainsDuplicate/php/solution1.php
@@ -1,2 +1,19 @@
 <?php
 
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Boolean
+     */
+    function containsDuplicate($nums) {
+        $a = [];
+
+        for ($i = 0; $i < count($nums); $i++) {
+            if(isset($a[$nums[$i]])) return true;
+            $a[$nums[$i]] = true;
+        }
+
+        return false;
+    }
+}
