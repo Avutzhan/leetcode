d4f08c01dd2a976f739922883b1f91a4f0fafb30
Avutzhan Dautov <dautov92@list.ru>
2021-02-18 17:43:55 +0600

    Move Zeroes php sol

--- Arrays/Easy/61-70/67.283MoveZeroes/php/solution1.php
+++ Arrays/Easy/61-70/67.283MoveZeroes/php/solution1.php
@@ -1,2 +1,23 @@
 <?php
 
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return NULL
+     */
+    function moveZeroes(&$nums) {
+        $pos = 0;
+
+        for ($i = 0; $i < count($nums); $i++) {
+            if ($nums[$i] !== 0) {
+                $nums[$pos++] = $nums[$i];
+            }
+        }
+
+        for ($i = $pos; $i < count($nums); $i++) {
+            $nums[$i] = 0;
+        }
+
+    }
+}
