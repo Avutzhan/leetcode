462705e7a2b100bf2ca2544bebc2827943b22b24
Avutzhan Dautov <dautov92@list.ru>
2021-02-18 17:53:44 +0600

    Best Time to Buy and Sell Stock II python sol

---
+++ Arrays/Easy/61-70/68.122BestTimetoBuyandSellStockII/python/solution1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0;
+
+        for i in range(1, len(prices)):
+            prev = prices[i - 1]
+            current = prices[i]
+
+            if prev < current:
+                profit += current - prev
+
+        return profit
