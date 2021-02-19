1190f0931eac181194a3cf0dcf15ed4d916e4677
Avutzhan Dautov <dautov92@list.ru>
2021-02-19 11:40:14 +0600

    Repeat from this commit

--- Arrays/Easy/71-80/73.448FindAllNumbersDisappearedinanArray/php/solution1.php
+++ Arrays/Easy/71-80/73.448FindAllNumbersDisappearedinanArray/php/solution1.php
@@ -1,2 +1,19 @@
 <?php
 
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer[]
+     */
+    function findDisappearedNumbers($nums) {
+        $result = [];
+        $count = count($nums);
+        $nums = array_count_values($nums);
+        for($i=1; $i<=$count; $i++){
+            if(!isset($nums[$i]))   $result[] = $i;
+        }
+
+        return $result;
+    }
+}
