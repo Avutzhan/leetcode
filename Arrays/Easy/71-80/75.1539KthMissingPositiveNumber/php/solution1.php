e15c26ca865c6fef4daf61d2700fa5830079283b
Avutzhan Dautov <dautov92@list.ru>
2021-02-19 12:07:46 +0600

    Kth Missing Positive Number php sol

--- Arrays/Easy/71-80/75.1539KthMissingPositiveNumber/php/solution1.php
+++ Arrays/Easy/71-80/75.1539KthMissingPositiveNumber/php/solution1.php
@@ -1,2 +1,15 @@
 <?php
 
+class Solution {
+
+    /**
+     * @param Integer[] $arr
+     * @param Integer $k
+     * @return Integer
+     */
+    function findKthPositive($arr, $k) {
+        $i = 0;
+        while ($i < count($arr) && $arr[$i] < $i + $k + 1) $i++;
+        return $i + $k;
+    }
+}
