cddd68947de540ff993b1f285a039a74d4fc667c
Avutzhan Dautov <dautov92@list.ru>
2021-02-19 11:46:03 +0600

    Two Sum II - Input array is sorted php sol

--- Arrays/Easy/71-80/74.167TwoSumII-Inputarrayissorted/php/solution1.php
+++ Arrays/Easy/71-80/74.167TwoSumII-Inputarrayissorted/php/solution1.php
@@ -1,2 +1,26 @@
 <?php
 
+class Solution {
+
+    /**
+     * @param Integer[] $numbers
+     * @param Integer $target
+     * @return Integer[]
+     */
+    function twoSum($numbers, $target) {
+        $l=count($numbers);
+        $i=0;
+        $j=$l-1;
+
+        while ($numbers[$i]+$numbers[$j] !== $target) {
+            if ($numbers[$i]+$numbers[$j] < $target) {
+                $i++;
+            } else {
+                $j--;
+            }
+
+        }
+
+        return [$i+1, $j+1];
+    }
+}
