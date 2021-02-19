9331f080cb32753623d04f6850ca030a7b479f05
Avutzhan Dautov <dautov92@list.ru>
2021-02-19 13:18:35 +0600

    Degree of an Array php sol

--- Arrays/Easy/71-80/77.697DegreeofanArray/php/solution1.php
+++ Arrays/Easy/71-80/77.697DegreeofanArray/php/solution1.php
@@ -1,2 +1,43 @@
 <?php
 
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function findShortestSubArray($nums) {
+        $counts = [];
+        $firstIndexes = [];
+        $lastIndexes = [];
+        $max = 0;
+
+        for ($i = 0; $i < count($nums); $i++) {
+            $k = $nums[$i];
+
+            if ($counts[$k] != NULL) {
+                $counts[$k] = $counts[$k] + 1;
+            } else {
+                $counts[$k] = 0 + 1;
+            }
+
+            $max = max($max, $counts[$k]);
+
+            if ($firstIndexes[$k] === NULL) {
+                $firstIndexes[$k] = $i;
+            }
+            $lastIndexes[$k] = $i;
+
+        }
+
+        $res = count($nums);
+
+        foreach ($counts as $k => $value) {
+            if ($counts[$k] === $max) {
+                $res = min($res, $lastIndexes[$k] - $firstIndexes[$k] + 1);
+            }
+        }
+
+        return $res;
+    }
+}
