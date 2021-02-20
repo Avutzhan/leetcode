1924f2dc1a0fc4637c1f944f214de221b949fb1a
Avutzhan Dautov <dautov92@list.ru>
2021-02-20 14:18:21 +0600

    Min Cost Climbing Stairs php sol

--- Arrays/Easy/81-90/87.746MinCostClimbingStairs/php/solution1.php
+++ Arrays/Easy/81-90/87.746MinCostClimbingStairs/php/solution1.php
@@ -1,2 +1,18 @@
 <?php
 
+class Solution {
+
+    /**
+     * @param Integer[] $cost
+     * @return Integer
+     */
+    function minCostClimbingStairs($A) {
+        $N = count($A);
+        $a = $A[0];
+        $b = $A[1];
+        $c = -1;
+        for ($i = 2; $i < $N; $i++, $a = $b, $b = $c)
+            $c = $A[$i] + min($a, $b);
+        return min($a, $b);
+    }
+}
