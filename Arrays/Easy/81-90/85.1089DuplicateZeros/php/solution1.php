90a3d6080e00f7f9d12f2370b4e4565a74792417
Avutzhan Dautov <dautov92@list.ru>
2021-02-20 14:02:43 +0600

    Duplicate Zeros php sol

--- Arrays/Easy/81-90/85.1089DuplicateZeros/php/solution1.php
+++ Arrays/Easy/81-90/85.1089DuplicateZeros/php/solution1.php
@@ -1,2 +1,18 @@
 <?php
 
+class Solution {
+
+    /**
+     * @param Integer[] $arr
+     * @return NULL
+     */
+    function duplicateZeros(&$arr) {
+        for ($i=0; $i<count($arr); $i++) {
+            if ($arr[$i] === 0) {
+                array_splice($arr, $i, 0, 0);
+                array_pop($arr);
+                $i+=1;
+            }
+        }
+    }
+}
