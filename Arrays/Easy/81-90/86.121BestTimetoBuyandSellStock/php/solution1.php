913e6ecfe923c8fa4ffc368809238ac807b8edd2
Avutzhan Dautov <dautov92@list.ru>
2021-02-20 14:10:33 +0600

    Best Time to Buy and Sell Stock php sol

--- Arrays/Easy/81-90/86.121BestTimetoBuyandSellStock/php/solution1.php
+++ Arrays/Easy/81-90/86.121BestTimetoBuyandSellStock/php/solution1.php
@@ -1,2 +1,18 @@
 <?php
 
+class Solution {
+
+    /**
+     * @param Integer[] $prices
+     * @return Integer
+     */
+    function maxProfit($prices) {
+        $min = INF; //max save integer 9007199254740991 or write just INF
+        $max = 0;
+        for ($i = 0; $i < count($prices); $i++) {
+            $min = min($min, $prices[$i]);
+            $max = max($max, $prices[$i] - $min);
+        }
+        return $max;
+    }
+}
