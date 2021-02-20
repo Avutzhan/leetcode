7ef035b734422fd7a9bbde05523c823ad01c390d
Avutzhan Dautov <dautov92@list.ru>
2021-02-20 19:01:22 +0600

    Check if Array Is Sorted and Rotated php sol

--- Arrays/Easy/81-90/89.1752CheckifArrayIsSortedandRotated/php/solution1.php
+++ Arrays/Easy/81-90/89.1752CheckifArrayIsSortedandRotated/php/solution1.php
@@ -1,2 +1,23 @@
 <?php
 
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Boolean
+     */
+    function check($nums) {
+        $decreased = false;
+
+        for ($i = 1; $i < count($nums); $i += 1) {
+            if ($nums[$i] < $nums[$i - 1]) {
+                if ($decreased) {
+                    return false;
+                }
+                $decreased = true;
+            }
+        }
+
+        return $decreased ? $nums[0] >= $nums[count($nums) - 1] : true;
+    }
+}
