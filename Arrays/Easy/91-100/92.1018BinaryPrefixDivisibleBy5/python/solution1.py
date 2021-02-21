ad30c2714797969e9a31a905b0ac610554ade053
Avutzhan Dautov <dautov92@list.ru>
2021-02-21 17:03:42 +0600

    Binary Prefix Divisible By 5 python sol

---
+++ Arrays/Easy/91-100/92.1018BinaryPrefixDivisibleBy5/python/solution1.py
@@ -0,0 +1,7 @@
+class Solution:
+    def prefixesDivBy5(self, A: List[int]) -> List[bool]:
+        ans, b = [], 0
+        for a in A:
+            b = b << 1 | a
+            ans.append(b % 5 == 0)
+        return ans
