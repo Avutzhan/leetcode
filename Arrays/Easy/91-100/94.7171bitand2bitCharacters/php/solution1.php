6126f4b00ffe7dd1b9b7fac8334543ca4fa277d4
Avutzhan Dautov <dautov92@list.ru>
2021-02-21 18:04:55 +0600

    1-bit and 2-bit Characters php sol

--- Arrays/Easy/91-100/94.7171bitand2bitCharacters/php/solution1.php
+++ Arrays/Easy/91-100/94.7171bitand2bitCharacters/php/solution1.php
@@ -1,2 +1,16 @@
 <?php
 
+class Solution {
+
+    /**
+     * @param Integer[] $bits
+     * @return Boolean
+     */
+    function isOneBitCharacter($bits) {
+        $isMidCharacter = false;
+        for($i = 0; $i < count($bits) - 1; $i++){
+            $isMidCharacter = ($bits[$i] === 1 && !$isMidCharacter);
+        }
+        return !$isMidCharacter;
+    }
+}
