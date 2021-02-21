f92aa7f1351529b95e6f341da2de0317807dbb91
Avutzhan Dautov <dautov92@list.ru>
2021-02-21 16:45:02 +0600

    Binary Prefix Divisible By 5 php sol

--- Arrays/Easy/91-100/92.1018BinaryPrefixDivisibleBy5/php/solution1.php
+++ Arrays/Easy/91-100/92.1018BinaryPrefixDivisibleBy5/php/solution1.php
@@ -1,2 +1,22 @@
 <?php
 
+class Solution {
+
+    /**
+     * @param Integer[] $A
+     * @return Boolean[]
+     */
+    function prefixesDivBy5($A) {
+        $cur = 0;
+        $r = [];
+
+        foreach ($A as $a) {
+            var_dump($a);
+            $cur = (($cur << 1) | $a) % 5;
+            array_push($r, $cur === 0);
+
+        }
+
+        return $r;
+    }
+}
