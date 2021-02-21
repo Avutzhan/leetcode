c4ee9c5bf9a8af4ee4cb798228bb90ce1eda8c38
Avutzhan Dautov <dautov92@list.ru>
2021-02-21 18:41:19 +0600

    Number of Equivalent Domino Pairs php sol

--- Arrays/Easy/91-100/96.1128NumberofEquivalentDominoPairs/php/solution1.php
+++ Arrays/Easy/91-100/96.1128NumberofEquivalentDominoPairs/php/solution1.php
@@ -1,2 +1,27 @@
 <?php
 
+class Solution {
+
+    /**
+     * @param Integer[][] $dominoes
+     * @return Integer
+     */
+    function numEquivDominoPairs($dominoes) {
+        $map = [];
+        $count = 0;
+
+        foreach ($dominoes as $domino) {
+            $key = $domino[0] < $domino[1] ? "{$domino[0]}{$domino[1]}" : "{$domino[1]}{$domino[0]}";
+
+            if (array_key_exists($key, $map)) {
+                $currentIndex = $map[$key] + 1;
+                $map[$key] = $currentIndex;
+                $count += $currentIndex;
+            } else {
+                $map[$key] = 0;
+            }
+        }
+
+        return $count;
+    }
+}
