4893f0ff72552a27348614ff9e9dc330d4a111b8
Avutzhan Dautov <dautov92@list.ru>
2021-02-22 14:42:02 +0600

    Contains Duplicate II php sol

--- Arrays/Easy/101-110/110.219ContainsDuplicateII/php/solution1.php
+++ Arrays/Easy/101-110/110.219ContainsDuplicateII/php/solution1.php
@@ -1,2 +1,21 @@
 <?php
 
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $k
+     * @return Boolean
+     */
+    function containsNearbyDuplicate($nums, $k) {
+        $ind = [];
+        $n = count($nums);
+        for ($i = 0; $i < $n; $i++) {
+            if (array_key_exists($nums[$i], $ind) && $i - $ind[$nums[$i]] <= $k) {
+                return true;
+            }
+            $ind[$nums[$i]] = $i;
+        }
+        return false;
+    }
+}
