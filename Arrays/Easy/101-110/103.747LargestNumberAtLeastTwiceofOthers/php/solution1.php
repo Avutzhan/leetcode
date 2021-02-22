81740a49e2845df49161bd8158415614290afaa8
Avutzhan Dautov <dautov92@list.ru>
2021-02-22 11:54:03 +0600

    Largest Number At Least Twice of Others php sol

--- Arrays/Easy/101-110/103.747LargestNumberAtLeastTwiceofOthers/php/solution1.php
+++ Arrays/Easy/101-110/103.747LargestNumberAtLeastTwiceofOthers/php/solution1.php
@@ -1,2 +1,24 @@
 <?php
 
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function dominantIndex($nums) {
+        $first = -INF;
+        $second = -INF;
+        $ans = 0;
+        for ($i = 0; $i < count($nums); $i++) {
+            if ($nums[$i] > $first) {
+                $second = $first;
+                $first = $nums[$i];
+                $ans = $i;
+            } else if ($nums[$i] > $second) {
+                $second = $nums[$i];
+            }
+        }
+        return $first >= $second * 2 ? $ans : -1;
+    }
+}
