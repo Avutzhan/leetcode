ce2003ed11a777a7df64fadaef69b3996436a93a
Avutzhan Dautov <dautov92@list.ru>
2021-02-22 17:29:09 +0600

    Valid Mountain Array php sol

--- Arrays/Easy/111-120/113.941ValidMountainArray/php/solution1.php
+++ Arrays/Easy/111-120/113.941ValidMountainArray/php/solution1.php
@@ -1,2 +1,17 @@
 <?php
 
+class Solution {
+
+    /**
+     * @param Integer[] $arr
+     * @return Boolean
+     */
+    function validMountainArray($A) {
+        $N = count($A);
+        $i = 0;
+        $j = $N - 1;
+        while ($i + 1 < $N  && $A[$i] < $A[$i + 1]) ++$i;
+        while (0 <= $j - 1 && $A[$j] < $A[$j - 1]) --$j;
+        return 0 < $i && $i == $j && $j < $N - 1;
+    }
+}
