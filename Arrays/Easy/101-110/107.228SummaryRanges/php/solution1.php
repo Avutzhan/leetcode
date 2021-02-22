b82b4350def942174760d5f0056f76ac0fe2dc44
Avutzhan Dautov <dautov92@list.ru>
2021-02-22 14:13:36 +0600

    Summary Ranges php sol

--- Arrays/Easy/101-110/107.228SummaryRanges/php/solution1.php
+++ Arrays/Easy/101-110/107.228SummaryRanges/php/solution1.php
@@ -1,2 +1,22 @@
 <?php
 
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return String[]
+     */
+    function summaryRanges($nums) {
+        $res = [];
+
+        $left = $nums[0];
+        for ($i = 0; $i < count($nums); $i++) {
+            if ($nums[$i] + 1 !== $nums[$i + 1]) {
+                array_push($res, $left === $nums[$i] ? '' . $nums[$i] : $left . '->' . $nums[$i]);
+
+                $left = $nums[$i + 1];
+            }
+        }
+        return $res;
+    }
+}
