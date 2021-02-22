d406b5489f55dbf1698ba12c72f28a0322c3008b
Avutzhan Dautov <dautov92@list.ru>
2021-02-22 11:00:05 +0600

    Find Pivot Index php sol

--- Arrays/Easy/91-100/100.724FindPivotIndex/php/solution1.php
+++ Arrays/Easy/91-100/100.724FindPivotIndex/php/solution1.php
@@ -1,2 +1,24 @@
 <?php
 
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function pivotIndex($nums) {
+        $sum = $reduce = array_reduce($nums, function($carry, $item) {
+            $carry += $item;
+            return $carry;
+        });
+
+        $sumL = 0;
+        for($i=0, $len=count($nums); $i<$len; $i++){
+            if ($sumL === ($sum-$nums[$i])/2) {
+                return $i;
+            }
+            $sumL += $nums[$i];
+        }
+        return -1;
+    }
+}
