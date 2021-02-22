869856c48c71c514ce7b70563d63afec9c04a39f
Avutzhan Dautov <dautov92@list.ru>
2021-02-22 16:05:05 +0600

    Check If N and Its Double Exist js sol

--- Arrays/Easy/111-120/111.1346CheckIfNandItsDoubleExist/js/solution1.js
+++ Arrays/Easy/111-120/111.1346CheckIfNandItsDoubleExist/js/solution1.js
@@ -2,16 +2,11 @@
  * @param {number[]} arr
  * @return {boolean}
  */
-var checkIfExist = function(A) {
-    const arr = [];
-
-    for (i = 0; i < A.length; i++) {
-        if (arr.indexOf(A[i] * 2) != -1 ||
-            arr.indexOf(A[i] / 2) != -1)
-            return true;                               // check prev - 1/2*,2* as it is not sorted
-
-        arr.push(A[i]);
+var checkIfExist = function(array) {
+    while (array.length) { // While the array has items
+        const n = array.pop() // Get an item and remove it from the array
+        // If the double or the half is found stop looking
+        if (array.includes(n*2) || array.includes(n/2)) return true
     }
-
-    return false;
+    return false // If we get here we've gone through the whole array without finding a match
 };
