c0f788abe9371df4a6466f7c63239b5fd5e36d9d
Avutzhan Dautov <dautov92@list.ru>
2021-02-22 14:05:34 +0600

    Plus One php sol

--- Arrays/Easy/101-110/106.66PlusOne/php/solution1.php
+++ Arrays/Easy/101-110/106.66PlusOne/php/solution1.php
@@ -1,2 +1,22 @@
 <?php
 
+class Solution {
+
+    /**
+     * @param Integer[] $digits
+     * @return Integer[]
+     */
+    function plusOne($digits) {
+        for($i = count($digits) - 1; $i >= 0; $i--) {
+            $digits[$i]++;
+            if ($digits[$i] > 9)  {
+                $digits[$i] = 0;
+            } else {
+                return $digits;
+            }
+        }
+        array_unshift($digits, 1);
+
+        return $digits;
+    }
+}
