8e75a838af64de36cd381a3ba4dfc5b8e826ec2b
Avutzhan Dautov <dautov92@list.ru>
2021-06-10 16:30:26 +0600

    breadth first search graphs

---
+++ Theory/Time Complexity/Searching Complexity/Breadth First Search/graph.py
@@ -0,0 +1,10 @@
+graph = {}
+graph["you"] = ["alice", "bob", "claire"]
+graph["bob"] = ["anuj", "peggy"]
+graph["alice"] = ["peggy"]
+graph["claire"] = ["thom", "johny"]
+graph["anuj"] = []
+graph["peggy"] = []
+graph["thom"] = []
+graph["johny"] = []
+
