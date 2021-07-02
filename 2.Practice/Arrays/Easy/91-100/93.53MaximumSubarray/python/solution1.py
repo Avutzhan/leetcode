d344dbfeb082ebacf5ee071de442f386badd81f9
sd3@vrcode.kz <sd3@vrcode.kz>
2021-07-02 15:24:23 +0600

    * history analytics * Maximum Subarray tracing

--- 2.Practice/Arrays/Easy/91-100/93.53MaximumSubarray/python/solution1.py
+++ 2.Practice/Arrays/Easy/91-100/93.53MaximumSubarray/python/solution1.py
@@ -4,3 +4,24 @@ class Solution:
             if nums[i-1] > 0:
                 nums[i] += nums[i-1]
         return max(nums)
+
+# input [-2,1,-3,4,-1,2,1,-5,4]
+#
+# 1) [1] -2 > 0 false
+# 2) [-3] 1 > 0 true enter
+# 3) -3 + 1 = 3 - 1 = -2
+# 4) [4] -2 > 0 false
+# 5) [-1] 4 > 0 true enter
+# 6) -1 + 4 = 4 - 1 = 3
+# 7) [2] 3 > 0 true enter
+# 8) 2 + 3 = 5
+# 9) [1] 5 > 0 true enter
+# 10) 1 + 5 = 6
+# 11) [-5] 6 > 0 true enter
+# 12) - 5 + 6 = 6 - 5 = -1
+# 13) [4] -1 > 0 false
+# 14) max([-2,1,-2,4,3,5,6,-1,4]) = 6
+#
+# output 6
+
+
