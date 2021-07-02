5ae525cc23a8fbdf757e7432680431426571b154
sd3@vrcode.kz <sd3@vrcode.kz>
2021-07-02 12:40:43 +0600

    * history analytics * search insertion position

---
+++ 2.Practice/Arrays/Easy/101-110/104.35SearchInsertPosition/python/solution2.py
@@ -0,0 +1,31 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        if target in nums:
+            return nums.index(target)
+        else:
+            k=0
+            for i in nums:
+                if i<target:
+                    k+=1
+                else:
+                    break
+            return k
+
+
+# input [1,3,5,6]
+# target 5
+#
+# 1) if target in nums true return index
+
+# input [1,3,6]
+# target 5
+#
+# 1) if target in nums false enter to else
+# 2) k = 0
+# 3) 1 < 5 true k+1 = 1
+# 4) 3 < 5 true k+1 = 2
+# 5) 6 < 5 false break
+# 6) just return k
+#
+# output 2
+
