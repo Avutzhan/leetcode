bae2541c6b26eb29510863c3494c5fcc3599852e
sd3@vrcode.kz <sd3@vrcode.kz>
2021-09-05 01:10:23 +0600

    Repeat 1480. Running Sum of 1d Array

--- 2.Practice/Arrays/Easy/1-10/1.1480.RunningSumof1dArray/python/solution1.py
+++ 2.Practice/Arrays/Easy/1-10/1.1480.RunningSumof1dArray/python/solution1.py
@@ -1,14 +1,10 @@
-##best solution simple
+##Best solution simple
 
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
-
         running = 0
         result = []
-
         for i in range(len(nums)):
-
             running += nums[i]
             result.append(running)
-
         return result
