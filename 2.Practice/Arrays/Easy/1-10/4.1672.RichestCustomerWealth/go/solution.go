d7f65fc2d4bf05aeb2f140abc093950342bb76a8
sd3@vrcode.kz <sd3@vrcode.kz>
2021-09-09 19:27:56 +0600

    1672. Richest Customer Wealth

---
+++ 2.Practice/Arrays/Easy/1-10/4.1672.RichestCustomerWealth/go/solution.go
@@ -0,0 +1,10 @@
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        wealth = []
+
+        for i in accounts:
+            wealth.append(sum(i))
+
+        return max(wealth)
+
+
