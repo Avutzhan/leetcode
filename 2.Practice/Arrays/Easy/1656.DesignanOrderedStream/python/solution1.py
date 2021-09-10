a1a4db4e549ac71ad709acbc3a26add9855a823f
sd3@vrcode.kz <sd3@vrcode.kz>
2021-09-10 13:50:18 +0600

    1389 1486 1656

--- 2.Practice/Arrays/Easy/1656.DesignanOrderedStream/python/solution1.py
+++ 2.Practice/Arrays/Easy/1656.DesignanOrderedStream/python/solution1.py
@@ -1,22 +1,16 @@
 class OrderedStream:
 
-    arr = []
-    index = 0
-
     def __init__(self, n: int):
         self.arr = [None for i in range(n + 1)]
         self.index = 0
-
-
 
     def insert(self, id: int, value: str) -> List[str]:
         self.arr[id - 1] = value
         result = []
         while self.arr[self.index] != None:
             result.append(self.arr[self.index])
-            self.index+=1
+            self.index += 1
         return result
-
 
 # Your OrderedStream object will be instantiated and called as such:
 # obj = OrderedStream(n)
