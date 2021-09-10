fd3ef59b16e218b5f93e4d2337a6ff442b51cb93
Avutzhan <dautov92@list.ru>
2021-09-10 14:11:47 +0600

    1534

--- 2.Practice/Arrays/Easy/11-20/11.1534.CountGoodTriplets/python/solution2.py
+++ 2.Practice/Arrays/Easy/11-20/11.1534.CountGoodTriplets/python/solution2.py
@@ -2,11 +2,9 @@ class Solution:
     def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
         res = 0
         n = len(arr)
-
         for i in range(n):
-            for j in range(i + 1, n - 1):
+            for j in range(i + 1, n): #тут был -1 можно и без него обойтись
                 for k in range(j + 1, n):
                     if (abs(arr[i] - arr[j]) <= a and abs(arr[j] - arr[k]) <= b and abs(arr[k] - arr[i]) <= c):
                         res += 1
-
         return res
