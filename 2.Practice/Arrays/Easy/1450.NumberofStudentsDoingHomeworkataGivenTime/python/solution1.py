de180997ebb3e449c79eefe4cff62d22ae83507d
Avutzhan <dautov92@list.ru>
2021-09-13 00:28:27 +0600

    832 1450 1572

--- 2.Practice/Arrays/Easy/1450.NumberofStudentsDoingHomeworkataGivenTime/python/solution1.py
+++ 2.Practice/Arrays/Easy/1450.NumberofStudentsDoingHomeworkataGivenTime/python/solution1.py
@@ -1,6 +1,6 @@
 class Solution:
     def busyStudent(self, startTime: List[int], endTime: List[int], queryTime: int) -> int:
-        res = 0;
+        res = 0
 
         for i in range(len(startTime)):
             if startTime[i] <= queryTime and queryTime <= endTime[i]:
