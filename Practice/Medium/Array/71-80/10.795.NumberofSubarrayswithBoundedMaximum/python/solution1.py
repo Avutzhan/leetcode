dfcbcb57fb246de095fc527ef8cca45b4662f24d
Avutzhan <dautov92@list.ru>
2021-09-19 20:29:09 +0600

    795. Number of Subarrays with Bounded Maximum

---
+++ Practice/Medium/Array/71-80/10.795.NumberofSubarrayswithBoundedMaximum/python/solution1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numSubarrayBoundedMax(self, A: List[int], L: int, R: int) -> int:
+        res, dp = 0, 0
+        prev = -1
+        for i in range(len(A)):
+            if A[i] < L and i > 0:
+                res += dp
+            if A[i] > R:
+                dp = 0
+                prev = i
+            if L <= A[i] <= R:
+                dp = i - prev
+                res += dp
+        return res
