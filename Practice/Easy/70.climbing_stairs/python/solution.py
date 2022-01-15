7b7bcf9cfe3349242903efaab18fa9381498bbff
avutzhan <dautov92@list.ru>
2022-01-15 21:23:28 +0600

    climbing stairs recursive + memoization

--- Practice/Easy/70.climbing_stairs/python/solution.py
+++ Practice/Easy/70.climbing_stairs/python/solution.py
@@ -1,4 +1,5 @@
 # DP but without array just used main idea of one dimensional dp one two
+# 1DP Bottom Up
 def climb_stairs(n: int) -> int:
     one, two = 1, 1
 
---
+++ Practice/Easy/70.climbing_stairs/python/solution3.py
@@ -0,0 +1,29 @@
+def climbing_stairs(n):
+    """
+    Recursion + Memoization(Top Down DP)
+    Time: Linear O(n)
+    Memory: O(Stack Size) = O(n)
+    :return:
+    """
+
+    def stairs(i, dp):
+        if i == 0:
+            return 1
+        if i < 0:
+            return 0
+
+        if dp[i] != -1:
+            return dp[i]
+
+        way_one = stairs(i - 1, dp)
+        way_two = stairs(i - 2, dp)
+
+        dp[i] = way_one + way_two
+        print(dp)
+        return dp[i]
+
+    dp = [-1] * (n + 1)
+    return stairs(n, dp)
+
+
+print(climbing_stairs(6))
