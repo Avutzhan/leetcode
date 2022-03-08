55648040a241950e5e5f14e704ca24ff35fb0876
avutzhan <dautov92@list.ru>
2022-03-09 00:31:57 +0600

    448. Find All Numbers Disappeared in an Array Sets time complexity

--- Practice/Easy/448.FindAllNumbersDisappearedinanArray/python/solution3.py
+++ Practice/Easy/448.FindAllNumbersDisappearedinanArray/python/solution3.py
@@ -3,6 +3,11 @@ class Solution:
     Time Complexity : O(n), we require O(n) time to insert all elements into hashset and another O(n)
     time to iterate over range and insert elements not present in hashset into ans, thus giving a total time of O(n).
     Space Complexity : O(n), required to maintain the hashset.
+    According to Python wiki: Time complexity, set is implemented as a hash table. So you can expect to
+    lookup/insert/delete in O(1) average. Unless your hash table's load factor is too high, then you face collisions
+    and O(n).
+    P.S. for some reason they claim O(n) for delete operation which looks like a mistype.
+    P.P.S. This is true for CPython, pypy is a different story.
     """
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
         s = set(nums)
