cb91876c8ed01ff9a061148e9428e2dc8b1932f6
avutzhan <dautov92@list.ru>
2022-03-10 17:08:46 +0600

    53. Maximum Subarray go

---
+++ Practice/Easy/53.MaximumSubarray/go/solution.go
@@ -0,0 +1,16 @@
+func maxSubArray(nums []int) int {
+	curSum, maxSum := nums[0], nums[0]
+
+	for i := 1; i < len(nums); i++ {
+		curSum = max(curSum+nums[i], nums[i])
+		maxSum = max(curSum, maxSum)
+	}
+	return maxSum
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
