403e8e2787243c76cbbe05d46b7ef4048b817dfd
avutzhan <dautov92@list.ru>
2022-05-27 23:39:18 +0600

    Find Two Numbers that Add up to "k"

--- Notes/4.RepeatDSpython/DataStructuresforCodingInterviewsinPython/1.lists/challenge-3.py
+++ Notes/4.RepeatDSpython/DataStructuresforCodingInterviewsinPython/1.lists/challenge-3.py
@@ -1,5 +1,6 @@
 def find_sum(lst, k):
-    # Write your code here Find Two Numbers that Add up to "k"
+    # Find Two Numbers that Add up to "k"
+    # Moving indices
     lst.sort()
     first = 0
     last = len(lst) - 1
@@ -16,3 +17,53 @@ def find_sum(lst, k):
             result.append(lst[last])
             return result
     return False
+
+
+def find_sum(lst, k):
+    # iterate lst with i Brute Force
+    for i in range(len(lst)):
+        # iterate lst with j
+        for j in range(len(lst)):
+            # if sum of two iterators is k
+            # and i is not equal to j
+            # then we have our answer
+            if(lst[i]+lst[j] is k and i is not j):
+                return [lst[i], lst[j]]
+
+
+print(find_sum([1, 2, 3, 4], 5))
+
+
+def binary_search(a, item):
+    first = 0
+    last = len(a) - 1
+    found = False
+    index = -1
+    while first <= last and not found:
+        mid = (first + last) // 2
+        if a[mid] == item:
+            index = mid
+            found = True
+        else:
+            if item < a[mid]:
+                last = mid - 1
+            else:
+                first = mid + 1
+    if found:
+        return index
+    else:
+        return -1
+
+
+def find_sum(lst, k):
+    lst.sort()
+    for j in range(len(lst)):
+        # find the difference in list through binary search
+        # return the only if we find an index
+        index = binary_search(lst, k - lst[j])
+        if index is not -1 and index is not j:
+            return [lst[j], k - lst[j]]
+
+
+print(find_sum([1, 5, 3], 2))
+print(find_sum([1, 2, 3, 4], 5))
