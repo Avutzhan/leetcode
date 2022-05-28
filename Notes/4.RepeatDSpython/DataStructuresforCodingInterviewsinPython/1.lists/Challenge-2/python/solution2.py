254591f5bd34ada190302cc79278bcf6db8ca2c8
avutzhan <dautov92@list.ru>
2022-05-28 20:48:16 +0600

    challenge 2 solution 2 bugfix

--- Notes/4.RepeatDSpython/DataStructuresforCodingInterviewsinPython/1.lists/Challenge-2/python/solution2.py
+++ Notes/4.RepeatDSpython/DataStructuresforCodingInterviewsinPython/1.lists/Challenge-2/python/solution2.py
@@ -1,24 +1,16 @@
 def merge(arr1, arr2):
-    """
-    In place solution
-    Time: O(n^2)
-    Space: O(1)
-    :param arr1:
-    :param arr2:
-    :return:
-    """
     index1 = 0
     index2 = 0
 
-    while (index1 < len(arr1)) and (index2 < len(arr2)):
-        if arr1[index1] < arr2[index2]:
+    while index1 < len(arr1) and index2 < len(arr2):
+        if arr1[index1] > arr2[index2]:
             arr1.insert(index1, arr2[index2])
             index1 += 1
             index2 += 1
         else:
             index1 += 1
 
-    while index2 < len(arr2):
+    if index2 < len(arr2):
         arr1.extend(arr2[index2:])
 
     return arr1
@@ -27,9 +19,6 @@ def merge(arr1, arr2):
 result = merge([1, 3, 4, 5], [2, 6, 7, 8])
 
 expected = [1, 2, 3, 4, 5, 6, 7, 8]
-
-result.sort()
-expected.sort()
 
 if result == expected:
     print("Success")
