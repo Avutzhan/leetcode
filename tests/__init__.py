46cb2e0503e227fbd416d54a23629396cf47e39a
avutzhan <dautov92@list.ru>
2023-04-10 21:03:23 +0600

    fix search import

--- tests/__init__.py
+++
@@ -1,2 +0,0 @@
-import sys
-sys.path.append('/home/avutzhan/python-projects/leetcode/Practice')
--- tests/test_search.py
+++ tests/test_search.py
@@ -1,15 +1,17 @@
-import __init__
-from search import *
 import unittest
+
+from Practice.search import linear_search, binary_search
 
 
 class TestSearch(unittest.TestCase):
 
     def test_linear_search(self):
+        self.assertEqual(linear_search([1, 2, 3], 5), -1)
         self.assertEqual(linear_search([1, 2, 3], 3), 2)
 
     def test_binary_search(self):
         self.assertEqual(binary_search([1, 2, 3], 5), -1)
+        self.assertEqual(binary_search([1, 2, 3], 3), 2)
 
 
 if __name__ == '__main__':
