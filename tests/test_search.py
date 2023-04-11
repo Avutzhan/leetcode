7b8be1b0ce8c80687385e60db9e905e93d7244af
avutzhan <dautov92@list.ru>
2023-04-11 22:23:44 +0600

    change binary search test cases

--- tests/test_search.py
+++ tests/test_search.py
@@ -6,12 +6,12 @@ from Practice.search import linear_search, binary_search
 class TestSearch(unittest.TestCase):
 
     def test_linear_search(self):
-        self.assertEqual(linear_search([1, 2, 3], 5), -1)
-        self.assertEqual(linear_search([1, 2, 3], 3), 2)
+        self.assertEqual(linear_search([-1, 0, 3, 5, 9, 12], 9), 4)
+        self.assertEqual(linear_search([-1, 0, 3, 5, 9, 12], 2), -1)
 
     def test_binary_search(self):
-        self.assertEqual(binary_search([1, 2, 3], 5), -1)
-        self.assertEqual(binary_search([1, 2, 3], 3), 2)
+        self.assertEqual(binary_search([-1, 0, 3, 5, 9, 12], 9), 4)
+        self.assertEqual(binary_search([-1, 0, 3, 5, 9, 12], 2), -1)
 
 
 if __name__ == '__main__':
